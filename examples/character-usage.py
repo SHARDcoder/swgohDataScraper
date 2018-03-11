59fde4a35ea85db151c239d8e629f35e4766857f
SHARDcoder <35032829+SHARDcoder@users.noreply.github.com>
2018-03-11 12:09:51 +0000

    Create character-usage.py

---
+++ examples/character-usage.py
@@ -0,0 +1,32 @@
+"""
+MIT License
+
+Copyright (c) 2018 SHARDcoder
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import swgoh
+
+bb8-overview = swgoh.characters().get_overview('BB 8')
+
+for i in bb8-overview:
+	print(i)
+
+print(swgoh.characters().get_health('BB 8', star = 4))
